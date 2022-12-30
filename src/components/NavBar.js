c348b6ff754cd03383b7db747846efeb2e12bdb2
stevenminken <stevenminken@gmail.com>
2022-12-30 12:29:49 +0100

    opdracht novi banana security uitwerking

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,62 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import logo from '../assets/banana-01.png';
-import { useHistory, Link } from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
+import {Link, useNavigate} from "react-router-dom";
+import './NavBar.css'
 
 function NavBar() {
-  const history = useHistory();
-
-  return (
-    <nav>
-        <Link to="/">
+    const navigate = useNavigate();
+    const {isAuth, user, logout} = useContext(AuthContext);
+    console.log("is auth: " + isAuth);
+    return (
+        <nav>
+            <Link to="/">
           <span className="logo-container">
             <img src={logo} alt="logo"/>
             <h3>
               Banana Security
             </h3>
           </span>
-        </Link>
+            </Link>
 
-      <div>
-        <button
-          type="button"
-          onClick={() => history.push('/signin')}
-        >
-          Log in
-        </button>
-        <button
-          type="button"
-          onClick={() => history.push('/signup')}
-        >
-          Registreren
-        </button>
-      </div>
-    </nav>
-  );
+            {!isAuth && (
+                <div>
+                    <button
+                        type="button"
+                        onClick={() => {
+                            navigate('/signin');
+                        }}
+                    >
+                        Log in
+                    </button>
+                    <button
+                        type="button"
+                        onClick={() => {
+                            navigate('/signup')
+                        }}
+                    >
+                        Registreren
+                    </button>
+                </div>)}
+            {isAuth && (
+                <div className="nav-div">
+                    <p className="nav-username">
+                        {user}
+                    </p>
+                    <button
+                        type="button"
+                        onClick={() => {
+                            navigate('/');
+                            logout();
+                            console.log("De gebruiker is uitgelogd");
+                        }}
+                    >
+                        Log Out
+                    </button>
+                </div>
+            )}
+        </nav>
+    );
 }
 
 export default NavBar;
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,20 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useContext, useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {AuthContext} from "../context/AuthContext";
 
 function SignIn() {
-  return (
-    <>
-      <h1>Inloggen</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
 
-      <form>
-        <p>*invoervelden*</p>
-        <button>Inloggen</button>
-      </form>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {login} = useContext(AuthContext);
+    const navigate = useNavigate();
 
-      <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
-    </>
-  );
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(`
+    Email: ${email}, 
+    Password: ${password}, 
+    `);
+        console.log(`email: ${email}, password: ${password}`);
+        login(email, password);
+        if (email !== '') {
+            navigate('/profile');
+        } else {
+            navigate('/');
+        }
+    }
+
+    return (
+        <>
+            <h1>Inloggen</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id
+                molestias qui quo unde?</p>
+
+            <form onSubmit={handleSubmit}>
+                <section>
+                    <label htmlFor="email-field">Email</label>
+                    <input
+                        name="email"
+                        id="email-field"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </section>
+                <section>
+                    <label htmlFor="password-field">Wachtwoord</label>
+                    <input
+                        name="password"
+                        id="password-field"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </section>
+                <button type="submit">Verstuur</button>
+            </form>
+
+            <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
+        </>
+    );
 }
 
 export default SignIn;
